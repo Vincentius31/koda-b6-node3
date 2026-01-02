5edeb29734b1f49459aa2da8a5624c7c247d2f55
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2026-01-02 16:16:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,11 @@
-function convertTanggal(inputTanggal){
-    let hari = ""
-    let bulan = ""
-    let tahun = ""
-    let valid = true
+import { convertTanggal } from "./lib/convert.js";
 
-    if(inputTanggal.length !== 10){
-        return "Format tanggal salah"
-    }
+process.stdout.write("Masukkan tanggal: ");
 
-    for(let i = 0; i<inputTanggal.length; i++){
-        if(i === 2 || i === 5){
-            if(inputTanggal[i] !== "-"){
-                valid = false
-                break
-            }
-        }
-        else{
-            if(inputTanggal[i] < "0" || inputTanggal[i] > "9"){
-                valid = false
-                break
-            }
-            
-            if(i<2){
-                hari += inputTanggal[i]
-            }
-            else if(i<5){
-                bulan += inputTanggal[i]
-            }
-            else{
-                tahun += inputTanggal[i]
-            } 
-        }
-    }
+process.stdin.on("data", function (data) {
+    let input = data.toString().trim();
+    let hasil = convertTanggal(input);
 
-    let hariInput = Number(hari)
-    let bulanInput = Number(bulan)
-
-    if(!valid || hariInput < 1 || hariInput > 31 || bulanInput < 1 || bulanInput > 12 || tahun.length !== 4){
-        return "Format tanggal salah"
-    }
-
-    return hari + "/" + bulan + "/" + tahun
-}
-
-console.log(convertTanggal())
+    console.log(hasil);
+    process.exit();
+});
